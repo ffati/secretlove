1a6a84868ecd1d7c8919f5662bbd03b508e1a17c
ffati <hellol2016@outlook.com>
2020-03-02 21:20:15 +0800

    主页功能

--- src/main/java/com/ff/controller/homePage/HomePageController.java
+++ src/main/java/com/ff/controller/homePage/HomePageController.java
@@ -4,6 +4,8 @@ import com.ff.entity.RegisterInnerFeelingEntity;
 import com.ff.entity.VicitorInnerFeelingEntity;
 import com.ff.service.registInnerFeeling.RegistInnerFeelingService;
 import com.ff.service.vicitorInnerFeeling.VicitorInnerFeelingService;
+import com.ff.util.common.CurrentUser;
+import com.ff.vo.CurrentUserVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.List;
 
 /**
  * @ClassName HomePageController
@@ -31,8 +36,24 @@ public class HomePageController {
     @Resource
     private RegistInnerFeelingService registInnerFeelingService;
 
+    @Autowired
+    private CurrentUser currentUserUtil;
+
     @RequestMapping("/homepage")
-    public String home(){
+    public String home(
+            HttpServletRequest request,
+            HttpSession httpSession,
+            Model model
+    ){
+
+/*        //取出当前用户
+        Principal principal = request.getUserPrincipal();
+        System.out.println(principal.getName());*/
+
+        if (currentUserUtil.currentUserIsAuthenticated()){
+            CurrentUserVo currentUserVo=currentUserUtil.currentUser(httpSession);
+            model.addAttribute("headSculpture",currentUserVo.getHeadPictureaddress());
+        }
 
         return "index";
     }
@@ -50,24 +71,52 @@ public class HomePageController {
  * @return: java.lang.String
  * 主页搜索
  */
+
+    /*    @PreAuthorize("isAuthenticated()")
+        @Secured("ROLE_ADMIN")*/
     @RequestMapping("/searchConfession")
     public String searchConfession(
+            HttpSession httpSession,
             Model model,
             @RequestParam(value = "searchParam")String param
     ){
 
-        VicitorInnerFeelingEntity vicitorInnerFeelingEntity= vicitorInnerFeelingService.searchVicitorFelling(param);
-        if (null!=vicitorInnerFeelingEntity){
-            model.addAttribute("showModel",vicitorInnerFeelingEntity);
+        if (currentUserUtil.currentUserIsAuthenticated()){
+            CurrentUserVo currentUserVo=currentUserUtil.currentUser(httpSession);
+            model.addAttribute("headSculpture",currentUserVo.getHeadPictureaddress());
+        }
+        long vicitorStartTime=System.currentTimeMillis();
+        List<VicitorInnerFeelingEntity> vicitorInnerFeelingEntityList= vicitorInnerFeelingService.searchVicitorFelling(param);
+        long vicitorEndTime=System.currentTimeMillis();
+        System.out.println("游客表执行的时间"+(vicitorEndTime-vicitorStartTime));
+
+        if (null!=vicitorInnerFeelingEntityList && vicitorInnerFeelingEntityList.size()!=0){
+
+            model.addAttribute("showModel",vicitorInnerFeelingEntityList.get(0));
         }else {
-            RegisterInnerFeelingEntity registerInnerFeelingEntity=registInnerFeelingService.searchRegisterFelling(param);
-            if (null!=registerInnerFeelingEntity){
-                model.addAttribute("showModel",registerInnerFeelingEntity);
-            }else {
+            long registerStartTime=System.currentTimeMillis();
+            List<RegisterInnerFeelingEntity> registerInnerFeelingEntityList=registInnerFeelingService.searchRegisterFelling(param);
+            long registerEndTime=System.currentTimeMillis();
+            System.out.println("注册表表执行的时间"+(registerEndTime-registerStartTime));
+
+            if (registerInnerFeelingEntityList.size()==1) {
+                if (null != registerInnerFeelingEntityList && registerInnerFeelingEntityList.get(0).getCustomized().equals("0")) {
+
+                    model.addAttribute("showModel", registerInnerFeelingEntityList.get(0));
+                    return "confession/confessionPage";
+                } else if (null != registerInnerFeelingEntityList && registerInnerFeelingEntityList.get(0).getCustomized().equals("1")) {
+
+                    model.addAttribute("showModel", registerInnerFeelingEntityList.get(0));
+                    return "individuation/individuation";
+                }
+            }else if (registerInnerFeelingEntityList.size()==0){
+
                 return "confession/lonelyPage";
             }
         }
+
         return "confession/confessionPage";
     }
+
 
 }
