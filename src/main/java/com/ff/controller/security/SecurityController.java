c532ce956b842c2b2d505eeea0865433aabd5df2
ffati <hellol2016@outlook.com>
2020-03-12 19:43:20 +0800

    登录bug

--- src/main/java/com/ff/controller/security/SecurityController.java
+++ src/main/java/com/ff/controller/security/SecurityController.java
@@ -1,15 +1,20 @@
 package com.ff.controller.security;
 
+import com.ff.util.common.CurrentUser;
+import com.ff.vo.CurrentUserVo;
 import com.ff.vo.Message;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.*;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.WebAttributes;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * @ClassName SecurityController
@@ -24,8 +29,22 @@ import javax.servlet.http.HttpServletRequest;
 @RequestMapping("/security")
 public class SecurityController {
 
+    @Autowired
+    private CurrentUser currentUserUtil;
+
+
     @RequestMapping("/accessDenied")
-    public String accessDenied(){
+    public String accessDenied(
+            HttpSession httpSession,
+            Model model
+        ){
+
+
+        if (currentUserUtil.currentUserIsAuthenticated()){
+            CurrentUserVo currentUserVo=currentUserUtil.currentUser(httpSession);
+            model.addAttribute("headSculpture",currentUserVo.getHeadPictureaddress());
+        }
+
         return "publicContent/permission";
     }
 
@@ -38,7 +57,7 @@ public class SecurityController {
         Message message=new Message();
 
         message.setStatusCode("477");
-        if (authenticationException instanceof UsernameNotFoundException || authenticationException instanceof BadCredentialsException) {
+        if (authenticationException instanceof UsernameNotFoundException || authenticationException instanceof BadCredentialsException||authenticationException instanceof InternalAuthenticationServiceException) {
             message.setInformation("用户名或密码错误");
         } else if (authenticationException instanceof DisabledException) {
             message.setInformation("用户已被禁用");
