ace5eae49035928edbc95f9cc99cb5d2d0a8e289
ffati <hellol2016@outlook.com>
2020-06-11 22:29:19 +0800

    userservice类

--- src/main/java/com/ff/service/user/UserService.java
+++ src/main/java/com/ff/service/user/UserService.java
@@ -1,8 +1,11 @@
 package com.ff.service.user;
 
 import com.ff.entity.RegisterUserEntity;
-import com.ff.entity.UserEntity;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * @ClassName UserService
@@ -16,10 +19,23 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UserService {
 
+   @Cacheable(cacheNames = "RegisterUser",key = "#registerUserEntity.userName")
    RegisterUserEntity findByAnyParameter(RegisterUserEntity registerUserEntity);
 
    Boolean insertRegisterUser(RegisterUserEntity registerUserEntity);
 
+   @CacheEvict(cacheNames = "RegisterUser",key = "#registerUserEntity.userName")
+   Boolean updateRegisterUser(RegisterUserEntity registerUserEntity);
+
    Integer findMaxID();
 
+   RegisterUserEntity findRegisterUserByUsername(String username);
+
+   Boolean checkPhoneNumber(String phoneNumber);
+
+   void addInfraction(String id);
+
+   List<RegisterUserEntity> findUserByPage(int size,int index);
+
+   int countUserNumber();
 }
--- src/main/java/com/ff/service/user/userIml/UserServiceImpl.java
+++ src/main/java/com/ff/service/user/userIml/UserServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * @ClassName UserServiceImpl
@@ -46,7 +47,61 @@ public class UserServiceImpl implements UserService {
 
 
     @Override
+    public Boolean updateRegisterUser(RegisterUserEntity registerUserEntity) {
+
+        Boolean flage=null;
+        try {
+            userDao.updateRegisterUser(registerUserEntity);
+            flage=true;
+        }catch (Exception e){
+            e.printStackTrace();
+            flage=false;
+        }
+        return flage;
+    }
+
+    @Override
     public Integer findMaxID() {
         return userDao.findMaxID();
+    }
+
+
+    @Override
+    public RegisterUserEntity findRegisterUserByUsername(String username) {
+        return userDao.findRegisterUserByUsername(username);
+    }
+
+    /*
+     * @author: ff
+     * @date: 2020/2/28 20:16
+     * @param: [phoneNumber]
+     * @return: java.lang.Boolean
+     * 手机查重true：已经注册
+     */
+    @Override
+    public Boolean checkPhoneNumber(String phoneNumber) {
+
+        if (null==userDao.checkPhoneNumber(phoneNumber)){
+
+            return false;
+
+        }
+
+        return true;
+    }
+
+    @Override
+    public void addInfraction(String id) {
+        userDao.addInfraction(id);
+    }
+
+    @Override
+    public List<RegisterUserEntity> findUserByPage(int size, int index) {
+        return userDao.findUserByPage(size,index);
+    }
+
+    @Override
+    public int countUserNumber() {
+        return userDao.countUserNumber();
     }
 }
